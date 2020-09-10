95e6c2ea1cfd66a457701de018b85371880ed1f4
Diego Dalairac <ddalairac@santandertecnologia.com.ar>
2020-09-10 18:44:49 -0300

    pasar sass y comentar estilos

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -6,7 +6,7 @@ import {TimerObservable} from "rxjs/observable/TimerObservable";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
